592b3bbe3bf4a417e446d506c2ba8064f6e8c2ce
DOF1109 <friasdaniel.1109@gmail.com>
2024-09-06 15:20:11 -0300

    Navbar complete

---
+++ components/Navbar.jsx
@@ -0,0 +1,22 @@
+import Link from "next/link"
+
+const Navbar = () => {
+    return(
+        <nav>
+            <h2>Navbar</h2>
+            <ul>
+            <li>
+                <Link href="/">Home</Link>
+            </li>
+            <li>
+                <Link href="/about">About</Link>
+            </li>
+            <li>
+                <Link href="/commerce/categories">Categories</Link>
+            </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
