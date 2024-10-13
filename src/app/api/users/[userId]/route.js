578de16fcd854e54bddfd5762fd856bfd3a02029
DOF1109 <friasdaniel.1109@gmail.com>
2024-10-13 15:57:11 -0300

    Search Params completed

--- src/app/api/users/[userId]/route.js
+++ src/app/api/users/[userId]/route.js
@@ -1,9 +1,17 @@
 import { NextResponse } from "next/server";
 
 export const GET = async (request, { params }) => {
+  // extract params from url
   const resp = await fetch(
     `https://jsonplaceholder.typicode.com/users/${params.userId}`
   ).then((res) => res.json());
+
+  // exctract search params or query from url
+  const { searchParams } = new URL(request.url);
+  console.log(searchParams.get("name"));
+  console.log(searchParams.get("last_name"));
+
+  // url for this example: http://localhost:3000/api/users/1?name=patroclo&last_name=messi
 
   return NextResponse.json(resp);
 };
